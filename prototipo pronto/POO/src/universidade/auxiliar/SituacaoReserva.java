c113829fa8b63d831574df7a64fdb5de9dbe249b
Luan Victor <107703846+yLu4n@users.noreply.github.com>
2023-12-03 16:16:48 -0300

    Delete prototipo pronto/POO directory

--- prototipo pronto/POO/src/universidade/auxiliar/SituacaoReserva.java
+++
@@ -1,7 +0,0 @@
-package universidade.auxiliar;
-
- enum SituacaoReserva {                                                  // Powered by: BRUNO CASÉ and ZAIRA DUTRA
-                                                                          //Last att: 12:58 pm  (21/11/2023)
-    APROVADA,
-    REPROVADA
-}
